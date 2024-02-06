97d75197b3753e8b257a9fe1fca9905aa8ced4c7
njayboehme <njboehme@gmail.com>
2024-02-06 13:44:25 -0800

    Workig mt

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -575,9 +575,9 @@ if __name__ == "__main__":
     
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=63) as executor:
-        eSensor = executor.submit(sensor.sensing_producer, sensor_bus, 0.05)
+        eSensor = executor.submit(sensor.sensing_producer, sensor_bus, 0.08)
         eInterpreter = executor.submit(inter.interpreter_consumer_producer, sensor_bus, inter_bus, 0.08)
-        eController = executor.submit(cont.controller_consumer, inter_bus, 0.1)
+        eController = executor.submit(cont.controller_consumer, inter_bus, 1.2)
     
     eSensor.result()
     eInterpreter.result()
